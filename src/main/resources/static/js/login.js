3cb5997f4060037283b9da751ad3dc83e2d63719
usuario <usuario@LAPTOP-2L53E4KM>
2026-05-19 16:21:54 -0500

    correcciones al header, nuevas imagenes

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,3 +1,5 @@
+import {showCalendar} from "./shared/calendar.js"
+
 document.addEventListener("DOMContentLoaded", () => {
     // Referencias DOM
     // -- CALENDARIO
@@ -9,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const passwordInput = document.querySelector('#password-input');
     const btnTogglePassword = document.querySelector(".btn-toggle-pass");
     
-    //CODIGO
+    //Funciones
     showCalendar(datePicker, displayDate, calendarTrigger);
     showAndHidePassword(passwordInput, btnTogglePassword);
 
@@ -32,35 +34,3 @@ function showAndHidePassword(passwordInput, btnTogglePassword) {
     }
 }
 
-// Mostrar Calendario
-function showCalendar(datePicker, displayDate, calendarTrigger) {
-    if (datePicker && displayDate) {
-        // Forzar la apertura del calendario al hacer clic en cualquier parte del bloque amarillo o blanco
-        if (calendarTrigger) {
-            calendarTrigger.addEventListener("click", () => {
-                try {
-                    datePicker.showPicker(); // Método oficial para abrir el selector de fecha
-                } catch (err) {
-                    console.log("El navegador no soporta showPicker, usando click alternativo");
-                    datePicker.click();
-                }
-            });
-        }
-
-        // Actualizar el texto cuando el usuario selecciona una fecha
-        datePicker.addEventListener("change", (e) => {
-            const date = new Date(e.target.value);
-
-            // Ajuste de zona horaria para evitar que se reste un día
-            date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-
-            const day = String(date.getDate()).padStart(2, '0');
-            const month = String(date.getMonth() + 1).padStart(2, '0');
-            const year = date.getFullYear();
-
-            if (!isNaN(date.getTime())) {
-                displayDate.textContent = `${day}/${month}/${year}`;
-            }
-        });
-    }
-}
