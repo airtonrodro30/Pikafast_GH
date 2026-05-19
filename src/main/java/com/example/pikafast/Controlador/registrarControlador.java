626614849c1557c868b8a94da8ae407ac4d048c3
airtonrodro30 <wawilo300@gmail.com>
2026-05-18 23:31:43 -0500

    CRUD usuarios y conexion MySQL

--- src/main/java/com/example/pikafast/Controlador/registrarControlador.java
+++ src/main/java/com/example/pikafast/Controlador/registrarControlador.java
@@ -1,13 +1,30 @@
 package com.example.pikafast.Controlador;
 
-
+import com.example.pikafast.model.Usuario;
+import com.example.pikafast.repository.UsuarioRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class registrarControlador {
-@GetMapping("/register")
+
+    private final UsuarioRepository repository;
+
+    public registrarControlador(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/register")
     public String mostrarFormularioRegistro(){
+
         return "register";
+    }
+
+    @PostMapping("/guardar-usuario")
+    public String guardarUsuario(Usuario usuario){
+
+        repository.save(usuario);
+
+        return "redirect:/login";
     }
 }
---
+++ src/main/java/com/example/pikafast/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.pikafast.repository;
+
+import com.example.pikafast.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository
+        extends JpaRepository<Usuario, Long> {
+
+}
