beea9fa91e01ac84595e9688197c7f8731d6c64d
airtonrodro30 <wawilo300@gmail.com>
2026-05-19 00:15:17 -0500

    Login funcional

--- src/main/java/com/example/pikafast/Controlador/LoginControlador.java
+++ src/main/java/com/example/pikafast/Controlador/LoginControlador.java
@@ -1,12 +1,54 @@
 package com.example.pikafast.Controlador;
 
+import com.example.pikafast.model.Usuario;
+import com.example.pikafast.repository.UsuarioRepository;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginControlador {
+
+    private final UsuarioRepository repository;
+
+    public LoginControlador(
+            UsuarioRepository repository) {
+
+        this.repository = repository;
+    }
+
     @GetMapping("/login")
     public String mostrarLogin() {
+
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String iniciarSesion(
+            @RequestParam String correo,
+            @RequestParam String password,
+            Model model) {
+
+        Usuario usuario =
+                repository.findByCorreoAndPassword(
+                        correo,
+                        password
+                );
+
+        if (usuario != null) {
+
+            System.out.println(
+                    usuario.getNombre()
+            );
+
+            return "redirect:/";
+        }
+
+        model.addAttribute(
+                "error",
+                "Correo o contraseña incorrectos"
+        );
 
         return "login";
     }
--- src/main/java/com/example/pikafast/repository/UsuarioRepository.java
+++ src/main/java/com/example/pikafast/repository/UsuarioRepository.java
@@ -6,4 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository
         extends JpaRepository<Usuario, Long> {
 
+        Usuario findByCorreoAndPassword(
+        String correo,
+        String password
+);
 }
+
