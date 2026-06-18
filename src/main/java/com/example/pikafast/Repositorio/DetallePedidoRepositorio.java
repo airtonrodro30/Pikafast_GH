3f327918d1f9aadf30e60388c049987a6dd68340
airtonrodro30 <wawilo300@gmail.com>
2026-06-18 01:36:50 -0500

    RF historial de pedidos y detalle de pedido

--- src/main/java/com/example/pikafast/Repositorio/DetallePedidoRepositorio.java
+++ src/main/java/com/example/pikafast/Repositorio/DetallePedidoRepositorio.java
@@ -1,7 +1,9 @@
 package com.example.pikafast.Repositorio;
 
 import com.example.pikafast.Entidad.DetallePedido;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DetallePedidoRepositorio extends JpaRepository<DetallePedido, Integer> {
+    List<DetallePedido> findByPedido_IdPedido(Integer idPedido);
 }
